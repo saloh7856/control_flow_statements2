25c7ed32e0fd896c9085bad37d9b458bea60bd5c
saloh7856 <salohiddinberdiyorov5@gmail.com>
2022-07-02 02:42:07 +0500

    3 ta son berilgan   ikkison wrtasidagi sonni topish masalan topish masalan a<b<c bu berda usha son b uzgaruvchiga mos keladi

--- bin/if03.dart
+++ bin/if03.dart
@@ -7,5 +7,17 @@ Args:
     c: Third number.
 Returns:
     int: return answer. */
-void main() {
+int func(int a,b,c){
+    if(a>b && a<c){
+            return a;
+        }else
+        if(a<b && b<c){
+            return b;
+        }else 
+        if(a>b && b>c){
+            return c;
+        }
+    }
+void main(){
+    print(func(16,14,20));
 }
