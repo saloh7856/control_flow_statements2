6644a1bb64705be25abe2f67e5e514ff03a6d49e
saloh7856 <salohiddinberdiyorov5@gmail.com>
2022-07-02 11:00:50 +0500

    5 xonali son berilgan son ichidan max digit  topish

--- bin/if05.dart
+++ bin/if05.dart
@@ -7,5 +7,34 @@ Returns:
     int: return answer.
 */
 
-void main() {
+int func(int n){
+    int x1,x2,x3,x4,x5;
+    x1=n%10;
+    n~/=10;
+    x2=n%10;
+    n~/=10;
+    x3=n%10;
+    n~/=10;
+    x4=n%10;
+    n~/=10;
+    x5=n%10;
+    if (x1>x2 && x1>x3 && x1>x4 && x1>x5){
+        return x1;
+    }
+    else if (x2>x1 && x2>x3 && x2>x4 && x2>x5){
+        return x2;
+    }
+    else if (x3>x2 && x3>x1 && x3>x4 && x3>x5){
+        return x3;
+    }
+    else if (x4>x2 && x4>x3 && x4>x1 && x4>x5){
+        return x4;
+    }
+    else if (x5>x2 && x5>x3 && x5>x4 && x5>x1){
+        return x5;
+    }
+}
+    
+void main(){
+    print(func(12568));
 }
