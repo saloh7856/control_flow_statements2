f871044513eedeb308c5086fadd28c9ba007c587
saloh7856 <salohiddinberdiyorov5@gmail.com>
2022-07-05 11:14:37 +0500

    if

--- bin/if03.dart
+++ bin/if03.dart
@@ -7,55 +7,16 @@ Args:
     c: Third number.
 Returns:
     int: return answer. */
-// int func(int a,b,c){
-//     if(a>b){
-//         if(a<c){
-//          return a;   
-//         }else
-//         if(c<b){
-//             return b;
-//         }
-        
-//     }else
-//     if(b>a){
-//         if(a>c){
-//             return a;
 
-//         }else
-//         if(b<c){
-//             return b;
-//         }
-//     }
-// }
-// void main(){
-//     print(func(16,18,20));
-// }
 int func(int a,b,c){
-    if(a>b){
-        if(a<c){
-         return a;   
-        }else
-        if(c<b){
-            return b;
-        }
-        
+    if(a>b && c>a){
+    return a;    
     }else
-    if(b>a){
-        if(a>c){
-            return a;
-
-        }else
-        if(b<c){
-            return b;
-        }else 
-        if(c<b){
-            if(c>a){
-                return c;
-            }
-        }
-    }
-    return 5;
+    if(b>a && c>b){
+        return b;
+    }else 
+    return c;
 }
 void main(){
-    print(func(19,18,20));
+    print(func(15,18,20));
 }
